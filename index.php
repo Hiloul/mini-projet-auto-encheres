9076bfc518f7bf40bb85481b5d040c78e944eec1
Hilel Hamidi <hilel.hamidi@gmail.com>
2022-11-06 11:11:05 +0100

    debut

--- index.php
+++ index.php
@@ -9,9 +9,34 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
     <title>Enchères Auto</title>
 </head>
 <body>
-    
+<nav>
+<a href="">Enchères</a>
+<a href="">Articles</a>
+<a href="">Vendre</a>
+</nav>    
+
+
+<section class="listAuto">
+<p>Mettre la liste des vehicules en vente</p>
+</section>
+
+<section class="actu">
+<p>Dernieres actualités du site ?</p>
+</section>
+
+
+
+
+
+
+
+<footer>
+    <p>&copy;AutoEnchères</p>
+</footer>
+
 </body>
 </html>
