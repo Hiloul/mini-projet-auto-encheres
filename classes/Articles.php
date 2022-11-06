a775446db9239733136667bf7f064d93a890148f
Hilel Hamidi <hilel.hamidi@gmail.com>
2022-11-06 19:29:10 +0100

    style

--- classes/Articles.php
+++ classes/Articles.php
@@ -1,6 +1,22 @@
-<link rel="stylesheet" href="style/style.css">
 
 
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Document</title>
+</head>
+<body>
 <h1>Articles</h1>
+    <a href="http://localhost/php/mini-projet-auto-encheres//index.php">Accueil</a>
+</body>
+</html>
 
-<a href="http://localhost/php/mini-projet-auto-encheres//index.php">Accueil</a>
+
--- classes/Ventes.php
+++ classes/Ventes.php
@@ -1,6 +1,19 @@
-<link rel="stylesheet" href="style/style.css">
 
 
 
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Document</title>
+</head>
+<body>
 <h1>Mettre en vente un vehicule</h1>
-
+</body>
+</html>
