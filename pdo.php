d59bf973809b332f4a6a9598f0fd76db164e52f7
Hilel Hamidi <hilel.hamidi@gmail.com>
2022-11-07 16:56:55 +0100

    ok

---
+++ pdo.php
@@ -0,0 +1,3 @@
+<?php
+
+$pdo= new PDO("mysql:dbname=auto-enchere;host=localhost", "root", "");
---
+++ supprimer_annonce.php
@@ -0,0 +1,17 @@
+<?php 
+require __DIR__."/pdo.php";
+
+if(isset($_GET["id"])){
+$query = $pdo->prepare("DELETE FROM `annonces` WHERE id=:id");
+$query->bindValue(':id', $_GET["id"], PDO::PARAM_INT);
+$resultat = $query->execute();
+if($resultat){
+    // echo "film supprimé";
+    header('Location: index.php');
+}else{
+    echo "erreur lors de la suppresion";
+}
+}else{
+    echo "erreur parametre id manquant dans l'url";
+}
+?>
