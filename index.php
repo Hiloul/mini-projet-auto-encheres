cfdb89db2bea87848eaa750f351e5ff4429c647a
Hilel Hamidi <hilel.hamidi@gmail.com>
2022-11-08 09:40:59 +0100

    navbaren include

--- index.php
+++ index.php
@@ -13,12 +13,8 @@
     <title>Enchères Auto</title>
 </head>
 <body>
-<nav>
-<a href="http://localhost/php/mini-projet-auto-encheres//index.php"><img src="img/logo1.png" id="logo" alt=""></a>
-<a href="liste_annonce.php">Enchères</a>
-<a href="classes/Articles.php">Articles</a>
-<a href="classes/Ventes.php">Vendre</a>
-</nav>    
+    
+<?php include('navbar.php'); ?> 
 
 
 
--- liste_annonce.php
+++ liste_annonce.php
@@ -1,10 +1,7 @@
 <link rel="stylesheet" href="style/style.css">
-<nav>
-<a href="http://localhost/php/mini-projet-auto-encheres//index.php"><img src="img/logo1.png" id="logo" alt=""></a>
-<a href="liste_annonce.php">Enchères</a>
-<a href="classes/Articles.php">Articles</a>
-<a href="classes/Ventes.php">Vendre</a>
-</nav>   
+
+<?php include('navbar.php'); ?> 
+  
 <?php
 require __DIR__."/pdo.php";
 
